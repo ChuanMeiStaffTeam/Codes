b389653bcf458151fa4278fff17acffa2aa2eb10
dong-guozhen <2029187514@qq.com>
2024-05-20 14:58:30 +0800

    CREATE PROJECT

---
+++ Server/HX/src/main/java/com/example/hx/HxApplication.java
@@ -0,0 +1,13 @@
+package com.example.hx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HxApplication.class, args);
+    }
+
+}
