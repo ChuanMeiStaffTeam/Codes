8a0c8637c3e9f434fb29a96e994180d853ec047b
dong-guozhen <2029187514@qq.com>
2024-05-30 09:48:39 +0800

    前后端统一返回对象处理,统一异常处理,统一配置处理,

---
+++ Server/HX/src/main/java/com/example/hx/exception/ApplicationException.java
@@ -0,0 +1,38 @@
+package com.example.hx.exception;
+
+
+import com.example.hx.common.AppResult;
+
+/**
+ * 自定义异常类
+ */
+public class ApplicationException extends RuntimeException{
+
+    //在异常中持有一个错误信息
+    private AppResult errorResult;
+
+    /**
+     * 构造方法
+     * @param errorResult
+     */
+    public ApplicationException (AppResult errorResult) {
+        super(errorResult.getMessage());
+        this.errorResult = errorResult;
+    }
+
+    public AppResult getErrorResult() {
+        return errorResult;
+    }
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ApplicationException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ Server/HX/src/main/java/com/example/hx/exception/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.example.hx.exception;
+
+
+import com.example.hx.common.AppResult;
+import com.example.hx.common.ResultCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理类
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(ApplicationException.class)   //表示要处理那种异常
+    public AppResult applicationExceptionHandler(ApplicationException e) {
+        //打印异常信息
+        e.printStackTrace();  //上生产之前删除
+        log.error(e.getMessage());
+
+        if(e.getErrorResult() != null) {
+            return e.getErrorResult();
+        }
+
+        //非空校验
+        if(e.getMessage() == null || e.getMessage().equals("")) {
+            return AppResult.failed(ResultCode.ERROR_SERVICES);
+        }
+        //返回具体异常信息
+        return AppResult.failed(e.getMessage());
+    }
+
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public AppResult exceptionHandler(Exception e) {
+        e.printStackTrace();  //上生产之前删除
+        log.error(e.getMessage());
+        //非空校验
+        if(e.getMessage() == null || e.getMessage().equals("")) {
+            return AppResult.failed(ResultCode.ERROR_SERVICES);
+        }
+
+        //返回具体异常信息
+        return AppResult.failed(e.getMessage());
+    }
+}
