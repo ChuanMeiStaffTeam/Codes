a866dde38e6ceb4bc7b099a498e967c382f29dcd
dong-guozhen <2029187514@qq.com>
2024-06-03 20:42:43 +0800

    用户

---
+++ Server/HX/src/main/java/com/example/hx/model/User.java
@@ -0,0 +1,16 @@
+package com.example.hx.model;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: DongGuoZhen
+ * @Date: 2024/06/03/20:15
+ * @Description:
+ */
+@Data
+public class User {
+
+
+}
