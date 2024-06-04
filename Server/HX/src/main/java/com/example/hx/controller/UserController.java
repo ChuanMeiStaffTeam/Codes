470edec39f9be837cdf09a86c7f5524d81ef24c9
dong-guozhen <2029187514@qq.com>
2024-06-04 21:04:36 +0800

    User

--- Server/HX/src/main/java/com/example/hx/controller/UserController.java
+++ Server/HX/src/main/java/com/example/hx/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.hx.controller;
 
+import com.example.hx.common.AppResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 
+    // 登录接口
+    @RequestMapping("/login")
+    public AppResult login(String username, String password) {
+        // TODO: 2024/05/20 登录逻辑
+        return AppResult.success();
+    }
+
+
+    // 注册接口
+    @RequestMapping("/register")
+    public AppResult register(String username, String password) {
+        // TODO: 2024/05/20 注册逻辑
+        return AppResult.success();
+    }
+
+    // 注销接口
+    @RequestMapping("/logout")
+    public AppResult logout() {
+        // TODO: 2024/05/20 注销逻辑
+        return AppResult.success();
+    }
 
 }
