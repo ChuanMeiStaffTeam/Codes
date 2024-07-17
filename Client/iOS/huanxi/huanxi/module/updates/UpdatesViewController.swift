24d35b7385c0b3f2b21e2d7129adc929cf9b1e8b
jack <869918615@qq.com>
2024-07-17 14:02:47 +0800

    修改动态页面

--- Client/iOS/huanxi/huanxi/module/updates/UpdatesViewController.swift
+++ Client/iOS/huanxi/huanxi/module/updates/UpdatesViewController.swift
@@ -9,6 +9,59 @@ import UIKit
 
 class UpdatesViewController: BaseViewController {
     
+    let vm = MainViewModel()
+    var mainView: MainView!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        
+        mainView.reloadMainViewData(vm.mainList)
+    }
+    
+    
+    func setupView() {
+        setupNavView()
+        
+        mainView = MainView(frame: CGRect.init(x: 0, y: 0, width: .screenWidth, height: .screenHeight - .bottomSafeAreaHeight - .tabBarHeight))
+        view.addSubview(mainView)
+        
+    }
+    
+    func setupNavView() {
+        
+        let imageView = UIImageView(image: UIImage.init(named: "instagram"))
+        imageView.frame = CGRect(x: 0, y: 4    , width: 134, height: 36)
+//        let leftItem = UIBarButtonItem(customView: imageView)
+        self.navigationItem.titleView = imageView
+        
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(x: .screenWidth - 46, y: 7, width: 30, height: 30)
+        button.setImage(UIImage.init(named: "main_relay"), for: .normal)
+        button.addTarget(self, action: #selector(gotoDirect), for: .touchUpInside)
+        let rightItem = UIBarButtonItem(customView: button)
+//        self.navigationItem.rightBarButtonItem = rightItem
+        
+    }
+    
+    
+    @objc func gotoDirect() {
+        let vc = DirectViewController()
+        vc.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
+
+/*
+class UpdatesViewController: BaseViewController {
+    
     let updatesThisMonthCell = "UpdatesThisMonthCell"
     let updatesEarlierCell = "UpdatesEarlierCell"
     let updatesRecommendCell = "UpdatesRecommendCell"
@@ -107,3 +160,5 @@ extension UpdatesViewController: UITableViewDelegate, UITableViewDataSource {
     
     
 }
+
+*/
