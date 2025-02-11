969ca5d35a78342154093ae6f2efa1e86fb13e29
rslz <rslzsy@gmail.com>
2025-02-11 17:15:17 +0800

    [fix]tabBar透明问题

--- Client/iOS/huanxi/huanxi/module/mine/MineViewController.swift
+++ Client/iOS/huanxi/huanxi/module/mine/MineViewController.swift
@@ -96,14 +96,6 @@ class MineViewController: BaseViewController {
         }
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        // TODO: - bug fix
-        if type == .mySelf {
-            hidesBottomBarWhenPushed = false
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.currentUser?.userId = userId
