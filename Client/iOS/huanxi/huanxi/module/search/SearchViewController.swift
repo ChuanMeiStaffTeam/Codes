8db44df88d489370a208c44db38965f50294ce3d
rslz <rslzsy@gmail.com>
2025-09-11 14:04:44 +0800

    [add]1.0.5

--- Client/iOS/huanxi/huanxi/module/search/SearchViewController.swift
+++ Client/iOS/huanxi/huanxi/module/search/SearchViewController.swift
@@ -61,26 +61,26 @@ class SearchViewController: BaseViewController {
             make.height.equalTo(UIDevice.sy_navigationFullHeight)
         }
         
-        searchTagsView.didSelectedItemCallBack = { [weak self] text in
-            guard let `self` = self else { return }
-            DispatchQueue.main.async {
-                let vc = SearchTagViewController()
-                vc.title = text
-                vc.hidesBottomBarWhenPushed = true
-                self.navigationController?.pushViewController(vc, animated: true)
-            }
-
-        }
-        view.addSubview(searchTagsView)
-        searchTagsView.snp.makeConstraints { make in
-            make.top.equalTo(searchHeaderView.snp.bottom)
-            make.leading.trailing.equalToSuperview()
-            make.height.equalTo(48)
-        }
+//        searchTagsView.didSelectedItemCallBack = { [weak self] text in
+//            guard let `self` = self else { return }
+//            DispatchQueue.main.async {
+//                let vc = SearchTagViewController()
+//                vc.title = text
+//                vc.hidesBottomBarWhenPushed = true
+//                self.navigationController?.pushViewController(vc, animated: true)
+//            }
+//
+//        }
+//        view.addSubview(searchTagsView)
+//        searchTagsView.snp.makeConstraints { make in
+//            make.top.equalTo(searchHeaderView.snp.bottom)
+//            make.leading.trailing.equalToSuperview()
+//            make.height.equalTo(48)
+//        }
         
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints { make in
-            make.top.equalTo(searchTagsView.snp.bottom).offset(5)
+            make.top.equalTo(searchHeaderView.snp.bottom).offset(5)
             make.leading.trailing.equalToSuperview()
             make.bottom.equalToSuperview().inset(UIDevice.sy_tabBarFullHeight)
         }
@@ -94,12 +94,12 @@ class SearchViewController: BaseViewController {
             })
             .disposed(by: disposeBag)
         
-        self.viewModel.postTagList
-            .subscribe(onNext: { [weak self] tags in
-                guard let `self` = self else { return }
-                self.searchTagsView.reloadData(tags)
-            })
-            .disposed(by: disposeBag)
+//        self.viewModel.postTagList
+//            .subscribe(onNext: { [weak self] tags in
+//                guard let `self` = self else { return }
+//                self.searchTagsView.reloadData(tags)
+//            })
+//            .disposed(by: disposeBag)
     }
 
 }
